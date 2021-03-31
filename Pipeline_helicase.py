655a9b6d28a5da192b9a17071ed79b8bfd5910f8
Victoria FATHI <victoria.fathi@gmail.com>
2021-03-31 23:15:45 +0200

    View multiple_status added and bug fixed

--- Pipeline_helicase.py
+++ Pipeline_helicase.py
@@ -5,49 +5,26 @@ import mysql.connector as mc
 import csv
 import argparse
 from configurations import config
-import urllib.parse
-import urllib.request
-import re
-import certifi
-import ssl
-
+import requests
+from requests.exceptions import HTTPError
 
 ################################################################################################
 parser = argparse.ArgumentParser(description='') 
 parser.add_argument('--helicasefile', '-f', type=str,
-                    help="tsv file containing CGBD id and associated helicase designated by its uniprot protein id")
-parser.add_argument('--drop', '-d', required=False, action="store_true", help='If needed to drop the tables first')
-'''
-parser.add_argument('--membersfile', '-m', dest="members_file", type=str,
-                    help="tsv file containing -> to complete (see readme from eggnog)")
-'''
+                    help="tsv file with CGBD id associated to uniprot protein id ")
+parser.add_argument('--arcogs', '-c', type = str, help = "tsv file with id_cogs descriptions and type from eggnog" )
+
+parser.add_argument('--drop', '-d', required=False, action="store_true", help='drop all tables')
 
 args = parser.parse_args()
 ###############################################################################################
 
-
-def conversion(initial, start='ACC', end='EGGNOG_ID'):
-    url = 'https://www.uniprot.org/uploadlists/'  # from here to the "response.decode('utf-8') it's a code i got
-    # from https://www.uniprot.org/help/api_idmapping.
-    params = {'from': start, 'to': end, 'format': 'tab', 'query': initial}
-
-    data = urllib.parse.urlencode(params)
-    data = data.encode('utf-8')
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req, context=ssl.create_default_context(cafile=certifi.where())) as f:
-        response = f.read()
-
-    result = re.findall("\t(\w{3,})\n", response.decode('utf-8'))
-
-    return result
-###############################################################################################
-
 try:
-    # Connection à la base de donnée
+    # try connection the database
     conn = mc.connect(host = 'localhost',
     database = 'ptut', 
-    user = config.BD_USER, # nom user dans le fichier config
-    password= config.BD_PASSWORD) # mot de passe dans le fichier config
+    user = config.BD_USER, # BD_USER by default in directroy configurations 
+    password= config.BD_PASSWORD) # BD_PASSEWORD by default in directory configurations
                 
 except mc.Error as err: # si la connexion échoue
     print(err)
@@ -58,28 +35,58 @@ else: # si le connexion réussie
 
     # Drop tables if new changes
     if args.drop :
-        cursor.execute("DROP TABLE `PROTEINS_COG`")
-        cursor.execute("DROP TABLE `COG`")
-        cursor.execute("DROP TABLE `MULTIPLE_STATUS`")
+        cursor.execute("DROP TABLE `proteins_cog`")
+        cursor.execute("DROP TABLE `cog`")
+        cursor.execute("DROP VIEW `multiple_status`")
 
     # Create tables if not exist
-    cursor.execute("CREATE TABLE IF NOT EXISTS `PROTEINS_COG`(`id_uniprot` VARCHAR(30),`id_cog` VARCHAR(100),PRIMARY KEY(`id_uniprot`, `id_cog`));")
-    cursor.execute("CREATE TABLE IF NOT EXISTS `COG`(`id_cog` VARCHAR(30) UNIQUE, `description` VARCHAR(100), `category` VARCHAR(30), `species_count` VARCHAR(10), `proteins_count` VARCHAR(10),PRIMARY KEY(`id_cog`));")
-    cursor.execute("CREATE TABLE IF NOT EXISTS `MULTIPLE_STATUS`(`client_id` VARCHAR(30), `id_uniprot`VARCHAR(30), `multiple`VARCHAR(30), `predicted_by`VARCHAR(30), PRIMARY KEY(`client_id`));")
+    cursor.execute("CREATE TABLE IF NOT EXISTS `proteins_cog`(`id_uniprot` VARCHAR(30),`id_cog` VARCHAR(100),PRIMARY KEY(`id_uniprot`, `id_cog`));")
+    cursor.execute("CREATE TABLE IF NOT EXISTS `cog`(`id_cog` VARCHAR(30) UNIQUE, `category` VARCHAR(100), `description` BLOB, PRIMARY KEY(`id_cog`));")
+
 
     with open(args.helicasefile, "r") as fh:  # reading tsv entry file using args module
-        tsv = csv.reader(fh, delimiter="\t")
-        for line in tsv:
-            arcog = conversion(line[1])
-            for el in arcog:
-                cursor.execute("INSERT INTO PROTEINS_COG (id_uniprot, id_cog) VALUES (%s,%s)", (line[1], el))
-            cursor.execute("INSERT INTO MULTIPLE_STATUS (client_id, id_uniprot, multiple, predicted_by) VALUES (%s,%s,%s,%s)", (line[0], line[1], len(arcog), 'NA'))
-       
+        tsv_helicases = csv.reader(fh, delimiter="\t")
+        for line in tsv_helicases:
+            print(line[1])
+            arcogs = []
+            try:
+                url = "https://www.uniprot.org/uniprot/"+line[1]+".txt" 
+                response = requests.get(url)
+                # If the response was successful, no Exception will be raised
+                response.raise_for_status()
+                file = response.text.split("\n")
+                for line_arc in file: 
+                    if line_arc.startswith("DR   eggNOG"):
+                        arcog = line_arc.split("; ")
+                        arcogs.append(arcog[1])
+                if arcogs:
+                    for el in arcogs:
+                        cursor.execute("INSERT INTO proteins_cog (id_uniprot, id_cog) VALUES (%s,%s)", (line[1], el))
+                else: 
+                    cursor.execute("INSERT INTO proteins_cog (id_uniprot, id_cog) VALUES (%s,%s)", (line[1], 'NA'))
 
+                
+            except HTTPError as http_err:
+                print(f'HTTP error occurred: {http_err}')  # Python 3.6
+            except Exception as err:
+                print(f'Other error occurred: {err}')  # Python 3.6
+        
+        cursor.execute("CREATE VIEW multiple_status AS SELECT id_uniprot, count(*) FROM PROTEINS_COG GROUP BY id_uniprot;")
+    
+    with open(args.arcogs, "r") as fa:
+        tsv_arcogs = csv.reader(fa, delimiter="\t")
+        for line in tsv_arcogs:
+            print(line[3])
+            if line[3] == "":
+                cursor.execute("INSERT INTO cog (id_cog, description, category) VALUES (%s,%s ,%s)", (line[1],'NA', line[2],))
+            else: 
+                cursor.execute("INSERT INTO cog (id_cog, description, category) VALUES (%s,%s ,%s)", (line[1], line[3], line[2]))
 
-
-finally: # s'execute de toute facon
-    conn.commit() # commit changes
+finally:
+    conn.commit()
     if(conn.is_connected()):
-        cursor.close() # ferme le cursor
-        conn.close() # ferme la connexion
+        cursor.close() # close cursor
+        conn.close() # close connection
+
+    
+          
