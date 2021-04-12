e55babb3897102d8db8e0f8d0be02a5f4058912a
Victoria FATHI <victoria.fathi@gmail.com>
2021-04-12 16:45:39 +0200

    Views added

--- Pipeline_helicase.py
+++ Pipeline_helicase.py
@@ -12,7 +12,7 @@ from requests.exceptions import HTTPError
 parser = argparse.ArgumentParser(description='') 
 parser.add_argument('--helicasefile', '-f', type=str,
                     help="tsv file with CGBD id associated to uniprot protein id ")
-parser.add_argument('--arcogs', '-c', type = str, help = "tsv file with id_cogs descriptions and type from eggnog" )
+parser.add_argument('--arcogs', '-c', type = str, required = False, help = "tsv file with id_cogs descriptions and type from eggnog" )
 
 parser.add_argument('--drop', '-d', required=False, action="store_true", help='drop all tables')
 
@@ -38,39 +38,44 @@ else: # si le connexion réussie
         cursor.execute("DROP TABLE `proteins_cog`")
         cursor.execute("DROP TABLE `cog`")
         cursor.execute("DROP VIEW `multiple_status`")
+        cursor.execute("DROP TABLE `strain_proteins`")
+        cursor.execute("DROP VIEW `paralogy`")
 
     # Create tables if not exist
     cursor.execute("CREATE TABLE IF NOT EXISTS `proteins_cog`(`id_uniprot` VARCHAR(30),`id_cog` VARCHAR(100),PRIMARY KEY(`id_uniprot`, `id_cog`));")
     cursor.execute("CREATE TABLE IF NOT EXISTS `cog`(`id_cog` VARCHAR(30) UNIQUE, `category` VARCHAR(100), `description` BLOB, PRIMARY KEY(`id_cog`));")
-
+    cursor.execute("CREATE TABLE IF NOT EXISTS `strain_proteins`(`strain` VARCHAR(30), `id_uniprot` VARCHAR(30),PRIMARY KEY(`id_uniprot`));")
 
     with open(args.helicasefile, "r") as fh:  # reading tsv entry file using args module
         tsv_helicases = csv.reader(fh, delimiter="\t")
         for line in tsv_helicases:
-            arcogs = []
+            strain = line[0].split(".")
+            strain = strain[0][0:5]
+            cursor.execute("INSERT INTO strain_proteins (strain, id_uniprot) VALUES (%s,%s)", (strain, line[1]))    
+            
+            arcogs = []    
             try:
                 url = "https://www.uniprot.org/uniprot/"+line[1]+".txt" 
                 response = requests.get(url)
                 # If the response was successful, no Exception will be raised
-                response.raise_for_status()
+                response.raise_for_status() #
                 file = response.text.split("\n")
                 for line_arc in file: 
                     if line_arc.startswith("DR   eggNOG"):
                         arcog = line_arc.split("; ")
                         arcogs.append(arcog[1])
-                if arcogs:
+                if arcogs: 
                     for el in arcogs:
                         cursor.execute("INSERT INTO proteins_cog (id_uniprot, id_cog) VALUES (%s,%s)", (line[1], el))
                 else: 
                     cursor.execute("INSERT INTO proteins_cog (id_uniprot, id_cog) VALUES (%s,%s)", (line[1], 'NA'))
-
                 
             except HTTPError as http_err:
                 print(f'HTTP error occurred: {http_err}')  # Python 3.6
             except Exception as err:
                 print(f'Other error occurred: {err}')  # Python 3.6
-        
-        cursor.execute("CREATE VIEW multiple_status AS SELECT id_uniprot, count(*) FROM proteins_cog GROUP BY id_uniprot;")
+
+    cursor.execute("CREATE VIEW multiple_status AS SELECT id_uniprot, count(*) AS multiple FROM proteins_cog GROUP BY id_uniprot;")
     
     with open(args.arcogs, "r") as fa:
         tsv_arcogs = csv.reader(fa, delimiter="\t")
@@ -79,6 +84,10 @@ else: # si le connexion réussie
                 cursor.execute("INSERT INTO cog (id_cog, description, category) VALUES (%s,%s ,%s)", (line[1],'NA', line[2],))
             else: 
                 cursor.execute("INSERT INTO cog (id_cog, description, category) VALUES (%s,%s ,%s)", (line[1], line[3], line[2]))
+    
+    
+    cursor.execute("CREATE VIEW paralogy AS SELECT id_cog, count(strain) AS strain_count, count(id_uniprot) as proteins_count FROM proteins_cog NATURAL JOIN strain_proteins GROUP BY id_cog;")
+
 finally:
     conn.commit()
     if(conn.is_connected()):
