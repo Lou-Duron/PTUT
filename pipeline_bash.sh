3d7d79103ff031b12e7047e383ef9a7c21d08bfd
Victoria FATHI <victoria.fathi@gmail.com>
2021-04-29 08:54:17 +0200

    installation of conda env and BD config

--- pipeline_bash.sh
+++ pipeline_bash.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 if [! command -v conda &> /dev/null]; then
-    echo "COMMAND could not be found"
+    printf "${red}conda could not be found "
+    printf "To install conda refers to https://conda.io/projects/conda/en/latest/user-guide/install/index.html${reset}"
     exit
 else
-    echo "Please enter the name of the environnment you want to create"
-    read -p 'Please Enter the name of your env ' nameEnv
-    echo "Creating your conda env " $nameEnv
+    echo "${green}Please enter the name of the environnment you want to create"
+    read -p "${green}Please Enter the name of your environnment${reset} " nameEnv
+    echo "${green}Creating your conda env $nameEnv ${reset}" 
     conda create --name $nameEnv --file installation/spec-list.txt
 fi
+
+mkdir configurations
+printf "${green}Need to enter username and password of mysql"
+printf "${green}Please enter your username\n${reset}" 
+read -i -e username
+printf "${green}Please enter your password\n${reset}"
+read -s password
+printf "#!/usr/bin/env python3\n# -*- coding: utf-8 -*-\nBD_USER = $username\nBD_PASSWORD = $password\n" > configuratio/test.txt
+printf "${green}if you need to change the user/password, follow this path${reset}\n"
+printf "configurations/config.py${reset}\n" 
+printf "- end of installation -\n"
 exit
