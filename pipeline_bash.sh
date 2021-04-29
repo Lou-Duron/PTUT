db58c6feeebd442cdae5a4120ccda90d1717a651
Victoria FATHI <victoria.fathi@gmail.com>
2021-04-29 12:28:58 +0200

    bash sript update

--- pipeline_bash.sh
+++ pipeline_bash.sh
@@ -14,29 +14,28 @@ else
     conda create --name $nameEnv --file installation/spec-list.txt
 fi
 
- # https://docs.fedoraproject.org/en-US/quick-docs/installing-mysql-mariadb/
-sudo dnf install https://repo.mysql.com//mysql80-community-release-fc31-1.noarch.rpm
-sudo dnf install mysql-community-server
-sudo systemctl start mysqld
-sudo systemctl enable mysqld
-sudo mysql_secure_installation
+if [! command -v mysql &> /dev/null]; then
+    printf "${red}Mysql could not be found "
+    printf "To install conda refers to https://dev.mysql.com/doc/mysql-installation-excerpt/5.7/en/${reset}"
+    exit
+else
+    mkdir results
+    mkdir data
+    mkdir configurations
 
-mkdir results
-mkdir data
-wget data/ http://eggnog5.embl.de/download/eggnog_5.0/per_tax_level/2157/2157_annotations.tsv.gz
-gunzip data/2157_annotations.tsv.gz
-
-mkdir configurations
-printf "${green}Need to enter username and password of mysql"
-printf "${green}Please enter your username\n${reset}" 
-read -i -e username
-printf "${green}Please enter your password\n${reset}"
-read -s password
-printf "#!/usr/bin/env python3\n# -*- coding: utf-8 -*-\nBD_USER = $username\nBD_PASSWORD = $password\n" > configurations/test.txt
-printf "${green}if you need to change the user/password, follow this path${reset}\n"
-printf "configurations/config.py${reset}\n" 
-printf "- end of installation -\n"
+    wget -O data/2157_annotations.tsv.gz http://eggnog5.embl.de/download/eggnog_5.0/per_tax_level/2157/2157_annotations.tsv.gz
+    tar -xvzf data/2157_annotations.tsv.gz
+    printf "${green}Need to enter username and password of mysql\n"
+    printf "${green}Please enter your username\n${reset}" 
+    read -i -e username
+    printf "${green}Please enter your password\n${reset}"
+    read -s password
+    printf "#!/usr/bin/env python3\n# -*- coding: utf-8 -*-\nBD_USER = $username\nBD_PASSWORD = $password\n" > configurations/config.txt
+    printf "${green}if you need to change the user/password, follow this path${reset}\n"
+    printf "configurations/config.py${reset}\n" 
+    printf "end of installation\n"
+fi
 exit
 
-exit
+
 
