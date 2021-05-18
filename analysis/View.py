ce4824b9518527524a267fcbb2b4d9a8f91d9b55
loudu <lou.duron@gmail.com>
2021-05-18 19:20:02 +0200

    View paralogy

--- analysis/View.py
+++ analysis/View.py
@@ -13,7 +13,6 @@ parser.add_argument('--name', '-n', type = str, help = "name of the view")
 parser.add_argument('--first', '-f', type = str, help = "name of the first table")
 parser.add_argument('--second', '-s', type = str, help = "name of the second table")
 parser.add_argument('--protein_nb', '-p', type=int, required=False, help="minimal number of protein for a given arcog")
-parser.add_argument('--p_value', '-v', type=float, required=False, help="minimal p_value of mapped arcgos")
 args = parser.parse_args()
 ###############################################################################################
 
@@ -29,13 +28,16 @@ else:
     cursor = conn.cursor()
     cursor.execute(f"USE {args.database}")
     cursor.execute(f"DROP VIEW IF EXISTS {args.name}")
-    request = f"CREATE VIEW {args.name} AS SELECT f.id_uniprot, f.id_cog FROM {args.first} f LEFT JOIN {args.second} s ON f.id_uniprot = s.id_uniprot " 
-    if args.protein_nb != None :
-        request += f"WHERE f.id_cog IN (SELECT id_cog FROM paralogy WHERE proteins_count > {str(args.protein_nb)})" 
-    request += f"UNION SELECT s.id_uniprot, s.id_cog FROM {args.first} f RIGHT JOIN {args.second} s ON f.id_uniprot = s.id_uniprot "
-    if args.protein_nb != None :
-        request += f"WHERE s.id_cog IN (SELECT id_cog FROM paralogy WHERE proteins_count > {str(args.protein_nb)})"
+    request = f"CREATE VIEW {args.name} AS SELECT f.id_uniprot, f.id_cog FROM proteins_cog_{args.first} f LEFT JOIN proteins_cog_{args.second} s ON f.id_uniprot = s.id_uniprot " 
+    if args.protein_nb is not None :
+        request += f"WHERE f.id_cog IN (SELECT id_cog FROM paralogy_{args.first} WHERE proteins_count > {str(args.protein_nb)})" 
+    request += f"UNION SELECT s.id_uniprot, s.id_cog FROM proteins_cog_{args.first} f RIGHT JOIN proteins_cog_{args.second} s ON f.id_uniprot = s.id_uniprot "
+    if args.protein_nb is not None :
+        request += f"WHERE s.id_cog IN (SELECT id_cog FROM paralogy_{args.second} WHERE proteins_count > {str(args.protein_nb)})"
     cursor.execute(request)
+    
+
+    # Infos
     cursor.execute(f"SELECT id_uniprot, id_cog FROM {args.name}")
     id_uniprot = []
     id_cog = []
